77ad5dbde23cc84141e2c096f942103601a4ae27
Drevinskyi Vladislav <drevinskiy.vlad@gmail.com>
2024-01-25 04:19:55 +0200

    Added before and after each hooks

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -50,7 +50,12 @@ export const config: Options.Testrunner = {
         timeout: 60000
     },
 
-    before: async function() {
+    beforeTest: async function() {
         await browser.setWindowSize(1920, 1080);
     },
+
+    afterTest: async function() {
+        await browser.deleteCookies();
+        await browser.reloadSession();
+    }
 }
