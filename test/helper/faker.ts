b57c108563ed0556511134d34d4a935f03b57649
drevinskiyvlad <114866402+drevinskiyvlad@users.noreply.github.com>
2024-01-29 00:11:51 +0000

    Deploying to gh-pages from @ drevinskiyvlad/rentzilla_practice@1866b8607737435f9abb68a95d0d5a2fb32a08eb 🚀

--- test/helper/faker.ts
+++
@@ -1,8 +0,0 @@
-export class Faker {
-    static generateRandomString(length: number): string {
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        const charactersLength = characters.length;
-        return Array.from({ length }).map(() =>
-            characters.charAt(Math.floor(Math.random() * charactersLength))).join('');
-    }
-}
