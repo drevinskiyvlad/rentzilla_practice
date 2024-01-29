a5e67f07ca66bbd7bd3e347937f2c24f0128e5d5
Drevinskyi Vladislav <drevinskiy.vlad@gmail.com>
2024-01-29 02:25:27 +0200

    some refactoring

--- test/pageobjects/header.ts
+++ test/pageobjects/header.ts
@@ -3,8 +3,8 @@ import BasePage from "./base-page.ts";
 const logo = 'a.Navbar_logo__RsJHS';
 const searchInput = 'header .MainSearch_input__Kr9pB';
 const firstSearchResult = '//header//div[@class=\'RightsideSearch_units__cojjw\']/div[1]';
-const firstSearchService = '//h6[@class=\'LeftsideSearch_title__FkeCp\'][2]/following-sibling::div[1]/div'
-const firstSearchCategory = '//h6[@class=\'LeftsideSearch_title__FkeCp\'][3]/following-sibling::div[1]/div'
+const firstSearchService = '//h6[@class=\'LeftsideSearch_title__FkeCp\'][2]/following-sibling::div[1]/div[1]'
+const firstSearchCategory = '//h6[@class=\'LeftsideSearch_title__FkeCp\'][3]/following-sibling::div[1]/div[1]'
 const cancelSearchButton = 'div[data-testid="searchClear"]';
 
 /**
